f8b5286cce2321d1e15219ec9040b19e9f456b83
Romeu Pires <rilpires@gmail.com>
2022-04-27 17:44:35 -0300

    proportional size on interval elements

--- lib/src/shape/interval.dart
+++ lib/src/shape/interval.dart
@@ -134,7 +134,16 @@ class RectShape extends IntervalShape {
 
           final start = coord.convert(item.position[0]);
           final end = coord.convert(item.position[1]);
-          final size = item.size ?? defaultSize;
+          // When item.size is between 0 and 1, we set it
+          // proportionally from the region available space 
+          double size = item.size ?? defaultSize;
+          if( size > 0.0 && size < 1.0 ){
+            if( coord.transposed )
+              size = coord.region.height * size;
+            else 
+              size = coord.region.width * size;
+          }
+          
           Rect rect;
           if (coord.transposed) {
             rect = Rect.fromLTRB(
